f2c407d1fad1b709effc679ac8e25b557f7d9582
= <=>
2024-03-16 15:15:11 +0530

    added meeting code

--- src/components/Collaborator.jsx
+++ src/components/Collaborator.jsx
@@ -5,6 +5,7 @@ import { WebrtcProvider } from "y-webrtc";
 import { MonacoBinding } from "y-monaco";
 import { executeCode } from '../api';
 import LanguageSelector from './LanguageSelector';
+import { CODE_SNIPPETS } from '../constants';
 import Output from './Output';
 
 function Collaborator() {
@@ -12,39 +13,53 @@ function Collaborator() {
   const [editorContent, setEditorContent] = useState("");
   const [value, setValue] = useState("");
   const [language, setLanguage] = useState("javascript");
-  
+  const [provider, setProvider] = useState(null);
+  const prevLanguage = useRef(language); // Track previous language for re-render
+
   const onSelect = (language) => {
     setLanguage(language);
     setValue(CODE_SNIPPETS[language]);
+    // Broadcast language change
+    const doc = provider.doc;
+    const langBinding = doc.getText('language');
+    langBinding.delete(0, langBinding.toString().length);
+    langBinding.insert(0, language);
   };
+
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
     const doc = new Y.Doc();
-    const provider = new WebrtcProvider("test-room", doc);
+    const newProvider = new WebrtcProvider("test-room", doc);
     const type = doc.getText("monaco");
-    console.log(type)
-    const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness);
-    console.log(provider.awareness);                
+    const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), newProvider.awareness);
+    setProvider(newProvider);
+
+    // Listen for language changes
+    const langBinding = doc.getText('language');
+    langBinding.observe(event => {
+      const newLanguage = event.changes.delta[0].retain;
+      monaco.editor.setModelLanguage(editorRef.current.getModel(), newLanguage);
+    });
   }
 
   const handleSaveButtonClick = async () => {
     const content = editorRef.current.getValue();
     setEditorContent(content);
-    const res = await executeCode(language,content);
+    const res = await executeCode(language, content);
     console.log(res);
-    
   };
 
   return (
     <div>
-        <LanguageSelector language={language} onSelect={onSelect}/>
-        <Output editorRef={editorRef} language={language}/>
+      <LanguageSelector language={language} onSelect={onSelect} provider={provider}/>
+      <Output editorRef={editorRef} language={language}/>
       <div>
         <Editor
           height="90vh"
           width="100vw"
           theme="vs-dark"
           onMount={handleEditorDidMount}
+          language={language} // Pass language prop to the editor
         />
       </div>
       <div>
